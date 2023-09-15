4dc8566306408a451e960bc53fe2462689c7c001
Sam Miller <smil@lle.rochester.edu>
2023-09-15 16:46:12 -0400

    Added a spherical 3d grid test

--- test/unit/test_3d.jl
+++ test/unit/test_3d.jl
@@ -79,3 +79,32 @@
   @test size(centroid_coords) == (3, 4, 8, 12)
   @test size(xyz_coords) == (3, 5, 9, 13)
 end
+
+@testitem "3D Mesh - Sphere Sector" begin
+  include("common.jl")
+
+  function sphere_grid(nr, ntheta, nphi)
+    r0, r1 = (1, 3)
+    (θ0, θ1) = deg2rad.((35, 180 - 35))
+    (ϕ0, ϕ1) = deg2rad.((45, 360 - 45))
+
+    r(ξ) = r0 + (r1 - r0) * ((ξ - 1) / (nr - 1))
+    θ(η) = θ0 + (θ1 - θ0) * ((η - 1) / (ntheta - 1))
+    ϕ(ζ) = ϕ0 + (ϕ1 - ϕ0) * ((ζ - 1) / (nphi - 1))
+
+    x(ξ, η, ζ) = r(ξ) * sin(θ(η)) * cos(ϕ(ζ))
+    y(ξ, η, ζ) = r(ξ) * sin(θ(η)) * sin(ϕ(ζ))
+    z(ξ, η, ζ) = r(ξ) * cos(θ(η))
+
+    x((ξ, η, ζ)) = x(ξ, η, ζ)
+    y((ξ, η, ζ)) = y(ξ, η, ζ)
+    z((ξ, η, ζ)) = z(ξ, η, ζ)
+
+    return (x, y, z)
+  end
+
+  ni, nj, nk = (5, 9, 11)
+  nhalo = 0
+  x, y, z = sphere_grid(ni, nj, nk)
+  @test_nowarn CurvilinearMesh3D(x, y, z, (ni, nj, nk), nhalo)
+end
