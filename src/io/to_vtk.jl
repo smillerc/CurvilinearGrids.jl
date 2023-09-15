de6baf819a7c289b8a257ee7cf9e37ad7fe8423d
Sam Miller <smil@lle.rochester.edu>
2023-09-15 16:46:34 -0400

    Add volume to vtk io output -- move this elsewhere in the future

--- src/io/to_vtk.jl
+++ src/io/to_vtk.jl
@@ -9,8 +9,14 @@ export to_vtk
 """Write the mesh to .VTK format"""
 function to_vtk(mesh::AbstractCurvilinearMesh, filename)
   xyz = coords(mesh)
+  cell_centered_data = zeros((mesh.nnodes .- 1)...)
+
+  @inbounds for I in CartesianIndices(cell_centered_data)
+    cell_centered_data[I] = abs(jacobian(mesh, I))
+  end
+
   vtk_grid(filename, xyz) do vtk
-    # add datasets...
+    vtk["volume"] = cell_centered_data
   end
 end
 
