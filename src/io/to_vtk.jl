ab1e7155cc3c552eff9afd76858dd5fa909915a9
Sam Miller <smil@lle.rochester.edu>
2023-09-22 15:14:36 -0400

    used mesh.J now instead of making a new volume array

--- src/io/to_vtk.jl
+++ src/io/to_vtk.jl
@@ -9,14 +9,9 @@ export to_vtk
 """Write the mesh to .VTK format"""
 function to_vtk(mesh::AbstractCurvilinearMesh, filename)
   xyz = coords(mesh)
-  cell_centered_data = zeros((mesh.nnodes .- 1)...)
-
-  @inbounds for I in CartesianIndices(cell_centered_data)
-    cell_centered_data[I] = abs(jacobian(mesh, I))
-  end
 
   vtk_grid(filename, xyz) do vtk
-    vtk["volume"] = cell_centered_data
+    vtk["volume"] = mesh.J
   end
 end
 
