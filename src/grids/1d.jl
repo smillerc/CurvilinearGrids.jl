5c7eb9c6b3c599a6689e106bd546c8d5fbf56139
Sam Miller <smil@lle.rochester.edu>
2023-10-16 17:36:48 -0400

    Removing type storage of metric terms -- emphasis compute rather than memory

--- src/grids/1d.jl
+++ src/grids/1d.jl
@@ -1,73 +1,97 @@
-struct CurvilinearGrid1D{T,T1,T2} <: AbstractCurvilinearGrid
+
+"""
+CurvilinearGrid1D
+
+# Fields
+ - `x` : Node function, e.g. x -> f(ξ)
+ - `∂x∂ξ` : Derivative of x wrt ξ; ∂x∂ξ(ξ)
+ - `nhalo` : Number of halo cells for all dims
+ - `nnodes` : Number of nodes/vertices
+ - `limits` : Cell loop limits based on halo cells
+ - `cell_center_metrics` : Grid metrics at each cell center
+ - `edge_metrics` : Grid metrics at each i+1/2 edge
+ - `J` : cell-centered Jacobian
+
+"""
+struct CurvilinearGrid1D{T1,T2} <: AbstractCurvilinearGrid
   x::T1 # x(ξ)
   ∂x∂ξ::T2 # ∂x∂ξ(ξ)
   nhalo::Int # number of halo cells (for all dimensions)
   nnodes::NTuple{1,Int}
   limits::NamedTuple{(:ilo, :ihi),NTuple{2,Int}}
-  cell_center_metrics::Vector{Metrics1D{T}}
-  edge_metrics::NamedTuple{(:ξ,),NTuple{
-    1, # only 1 dimension (ξ)
-    Vector{Metrics1D{T}}, # an array to hold the metrics (ξx, ξt)
-  }}
+  # cell_center_metrics::Vector{Metrics1D{T}}
+  # edge_metrics::NamedTuple{(:ξ,),NTuple{
+  #   1, # only 1 dimension (ξ)
+  #   Vector{Metrics1D{T}}, # an array to hold the metrics (ξx, ξt)
+  # }}
 
-  J::Vector{T} # cell-centered Jacobian J
+  # J::Vector{T} # cell-centered Jacobian J
 end
 
+"""
+    CurvilinearGrid1D(x::Function, (n_ξ,), nhalo)
+
+Create a `CurvilinearGrid1D` with a function `x(ξ)` and `nhalo` halo cells. `n_ξ` is the 
+total number of nodes/vertices (not including halo).
+"""
 function CurvilinearGrid1D(x::Function, (n_ξ,), nhalo)
   ∂x∂ξ(ξ) = ForwardDiff.derivative(x, ξ)
 
   nnodes = (n_ξ,)
   ni_cells = n_ξ - 1
   lo = nhalo + 1
-  cell_dims = (ni_cells,)
+  # cell_dims = (ni_cells,)
+  # cell_dims_whalo = cell_dims .+ 2nhalo
+  limits = (ilo=lo, ihi=ni_cells + nhalo)
 
-  limits = (ilo=lo, ihi=ni_cells - nhalo)
+  # cell_center_metrics = _make_empty_metric_array(cell_dims_whalo)
 
-  cell_center_metrics = _make_empty_metric_array(cell_dims)
+  # # metric terms for the i±1/2 edges
+  # edge_metrics = (ξ=_make_empty_metric_array(cell_dims_whalo),)
 
-  # metric terms for the i±1/2 edges
-  edge_metrics = (ξ=_make_empty_metric_array(cell_dims),)
-
-  J = zeros(cell_dims)
+  # J = zeros(cell_dims_whalo)
   m = CurvilinearGrid1D(
-    x, ∂x∂ξ, nhalo, nnodes, limits, cell_center_metrics, edge_metrics, J
+    x,
+    ∂x∂ξ,
+    nhalo,
+    nnodes,
+    limits,
+    # cell_center_metrics, edge_metrics, J
   )
-
-  # initialize the metric terms
-  update(m)
 
   return m
 end
 
-function update(m::CurvilinearGrid1D)
-  @inline for i in eachindex(m.J)
-    metric = (ξ̂x=m.∂x∂ξ(i), ξt=0.0)
-    edge_metric = (ξ̂x=m.∂x∂ξ(i + 0.5), ξt=0.0)
-    m.cell_center_metrics[i] = metric
-    m.edge_metrics.ξ[i] = edge_metric
-  end
+function conservative_metrics(m::CurvilinearGrid1D, i)
+  ξx = m.∂x∂ξ(i)
+  return (ξ̂x=1 / (ξx * ξx), ξt=zero(eltype(ξx)))
+end
 
-  @inbounds for i in eachindex(m.J)
-    m.J[i] = m.∂x∂ξ(i)
-  end
+function metrics(m::CurvilinearGrid1D, i)
+  ξx = m.∂x∂ξ(i)
+  return (ξx=1 / ξx, ξt=zero(eltype(ξx)))
+end
 
-  return nothing
+function metrics(m::CurvilinearGrid1D, i, vx)
+  static_metrics = metrics(m, i)
+  return merge(static_metrics, (ξt=-(vx * ξx),))
+end
+
+function jacobian_matrix(m::CurvilinearGrid1D, i)
+  return SMatrix{1,1}(m.∂x∂ξ(i))
 end
 
 function centroids(m::CurvilinearGrid1D)
-  x = zeros(m.nnodes .- 1)
-  for i in axes(x, 1)
+  x = zeros(cellsize(m))
+  ilo, ihi = m.limits
+  for i in ilo:ihi
     x[i] = m.x(i + 0.5)
   end
 
   return x
 end
 
-function coords(m::CurvilinearGrid1D)
-  x = zeros(m.nnodes)
-  for i in axes(x, 1)
-    x[i] = m.x(i)
-  end
-
-  return x
+function coords(mesh::CurvilinearGrid1D)
+  @unpack ilo, ihi = mesh.limits
+  return mesh.x.(ilo:(ihi + 1))
 end
--- test/unit/test_1d.jl
+++ test/unit/test_1d.jl
@@ -1,7 +1,26 @@
 
-using CurvilinearGrids
+@testitem "1D Mesh - Rectlinear Mesh" begin
+  include("common.jl")
 
-ni = 10
-nhalo = 2
-x(i) = i^2
-m = CurvilinearGrid1D(x, (ni,), nhalo)
+  function LinearSpacing((x0, x1), ni, nhalo)
+    _x(i) = x0 + (x1 - x0) * ((i - 1) / (ni - 1))
+    x(i) = _x(i - nhalo)
+    return x
+  end
+
+  function getmesh()
+    nhalo = 0
+    ni = 5
+    x0, x1 = (0.0, 2.0)
+    x = LinearSpacing((x0, x1), ni, nhalo)
+    return CurvilinearGrid1D(x, ni, nhalo)
+  end
+
+  m = getmesh()
+  @test metrics(m, 1) == (ξx=2.0, ξt=0.0)
+  @test conservative_metrics(m, 1) == (ξ̂x=4.0, ξt=0.0)
+  @test jacobian_matrix(m, 1) == @SMatrix [0.5]
+  @test jacobian(m, 1) == 0.5
+  @test inv(jacobian_matrix(m, 1)) == @SMatrix [2.0]
+  @test inv(jacobian(m, 1)) == 2.0
+end
