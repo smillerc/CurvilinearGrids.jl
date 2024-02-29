dbdf7e20398b202338e5e83c9341dc4726228b8d
Sam Miller <smil@lle.rochester.edu>
2024-02-29 16:40:12 -0500

    remove halo region iterators; these are easier to construct with the indexing utils

--- src/grids/grid_iterators.jl
+++ src/grids/grid_iterators.jl
@@ -9,24 +9,6 @@ function get_node_cell_iterators(
         (begin + nhalo):(end - nhalo),
         (begin + nhalo):(end - nhalo),
       ],
-      ilo_halo=nodeCI[
-        begin:(begin + nhalo), (begin + nhalo):(end - nhalo), (begin + nhalo):(end - nhalo)
-      ],
-      jlo_halo=nodeCI[
-        (begin + nhalo):(end - nhalo), begin:(begin + nhalo), (begin + nhalo):(end - nhalo)
-      ],
-      klo_halo=nodeCI[
-        (begin + nhalo):(end - nhalo), (begin + nhalo):(end - nhalo), begin:(begin + nhalo)
-      ],
-      ihi_halo=nodeCI[
-        (end - nhalo):end, (begin + nhalo):(end - nhalo), (begin + nhalo):(end - nhalo)
-      ],
-      jhi_halo=nodeCI[
-        (begin + nhalo):(end - nhalo), (end - nhalo):end, (begin + nhalo):(end - nhalo)
-      ],
-      khi_halo=nodeCI[
-        (begin + nhalo):(end - nhalo), (begin + nhalo):(end - nhalo), (end - nhalo):end
-      ],
     ),
     cell=(
       full=cellCI,
@@ -34,30 +16,6 @@ function get_node_cell_iterators(
         (begin + nhalo):(end - nhalo),
         (begin + nhalo):(end - nhalo),
         (begin + nhalo):(end - nhalo),
-      ],
-      ilo_halo=cellCI[
-        begin:(begin + nhalo - 1),
-        (begin + nhalo):(end - nhalo),
-        (begin + nhalo):(end - nhalo),
-      ],
-      jlo_halo=cellCI[
-        (begin + nhalo):(end - nhalo),
-        begin:(begin + nhalo - 1),
-        (begin + nhalo):(end - nhalo),
-      ],
-      klo_halo=cellCI[
-        (begin + nhalo):(end - nhalo),
-        (begin + nhalo):(end - nhalo),
-        begin:(begin + nhalo - 1),
-      ],
-      ihi_halo=cellCI[
-        (end - nhalo + 1):end, (begin + nhalo):(end - nhalo), (begin + nhalo):(end - nhalo)
-      ],
-      jhi_halo=cellCI[
-        (begin + nhalo):(end - nhalo), (end - nhalo + 1):end, (begin + nhalo):(end - nhalo)
-      ],
-      khi_halo=cellCI[
-        (begin + nhalo):(end - nhalo), (begin + nhalo):(end - nhalo), (end - nhalo + 1):end
       ],
     ),
   )
@@ -72,18 +30,10 @@ function get_node_cell_iterators(
     node=(
       full=nodeCI,
       domain=nodeCI[(begin + nhalo):(end - nhalo), (begin + nhalo):(end - nhalo)],
-      ilo_halo=nodeCI[begin:(begin + nhalo), (begin + nhalo):(end - nhalo)],
-      ihi_halo=nodeCI[(end - nhalo):end, (begin + nhalo):(end - nhalo)],
-      jlo_halo=nodeCI[(begin + nhalo):(end - nhalo), begin:(begin + nhalo)],
-      jhi_halo=nodeCI[(begin + nhalo):(end - nhalo), (end - nhalo):end],
     ),
     cell=(
       full=cellCI,
       domain=cellCI[(begin + nhalo):(end - nhalo), (begin + nhalo):(end - nhalo)],
-      ilo_halo=cellCI[begin:(begin + nhalo - 1), (begin + nhalo):(end - nhalo)],
-      jlo_halo=cellCI[(begin + nhalo):(end - nhalo), begin:(begin + nhalo - 1)],
-      ihi_halo=cellCI[(end - nhalo + 1):end, (begin + nhalo):(end - nhalo)],
-      jhi_halo=cellCI[(begin + nhalo):(end - nhalo), (end - nhalo + 1):end],
     ),
   )
   return _iterators
@@ -93,18 +43,8 @@ function get_node_cell_iterators(
   nodeCI::CartesianIndices{1}, cellCI::CartesianIndices{1}, nhalo
 )
   _iterators = (
-    node=(
-      full=nodeCI,
-      domain=nodeCI[(begin + nhalo):(end - nhalo)],
-      ilo_halo=nodeCI[begin:(begin + nhalo)],
-      ihi_halo=nodeCI[(end - nhalo):end],
-    ),
-    cell=(
-      full=cellCI,
-      domain=cellCI[(begin + nhalo):(end - nhalo)],
-      ilo_halo=cellCI[begin:(begin + nhalo - 1)],
-      ihi_halo=cellCI[(end - nhalo + 1):end],
-    ),
+    node=(full=nodeCI, domain=nodeCI[(begin + nhalo):(end - nhalo)]),
+    cell=(full=cellCI, domain=cellCI[(begin + nhalo):(end - nhalo)]),
   )
   return _iterators
 end
