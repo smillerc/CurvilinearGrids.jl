5f2cf3a43161866ee195a2cfecbc955201f2da1d
Miller, Samuel <smil@lle.rochester.edu>
2024-06-12 15:57:04 -0400

    test/update finite difference metric scheme

--- src/metric_schemes/finitediff_metrics.jl
+++ src/metric_schemes/finitediff_metrics.jl
@@ -1,28 +1,44 @@
+module FiniteDifferenceMetrics
 
 using FiniteDifferences
+using StaticArrays
+using Polyester
+using LinearAlgebra
 
-function _compute_fd_metrics(mesh)
-  xyn = coords(mesh)
-  xn = @view xyn[1, :, :]
-  yn = @view xyn[2, :, :]
-  ∂x_∂ξ, ∂x_∂η = _fd_metrics(xn, 4)
-  ∂y_∂ξ, ∂y_∂η = _fd_metrics(yn, 4)
+function _compute_fd_metrics(coordinates, order)
+  # xyn = coords(mesh)
+  xn = coordinates[1]
+  yn = coordinates[2]
+  ∂x_∂ξ, ∂x_∂η = _fd_metrics(xn, order)
+  ∂y_∂ξ, ∂y_∂η = _fd_metrics(yn, order)
+
+  ∂ξ_∂x = similar(xn)
+  ∂η_∂x = similar(xn)
+  ∂ξ_∂y = similar(xn)
+  ∂η_∂y = similar(xn)
 
   jacobian = similar(xn)
 
-  for idx in jacobian
+  @batch for idx in eachindex(jacobian)
     jacobian_matrix = @SMatrix [
       ∂x_∂ξ[idx] ∂y_∂ξ[idx]
       ∂x_∂η[idx] ∂y_∂η[idx]
     ]
 
+    inv_jacobian_matrix = inv(jacobian_matrix)
+
     jacobian[idx] = det(jacobian_matrix)
+
+    ∂ξ_∂x[idx] = inv_jacobian_matrix[1, 1]
+    ∂ξ_∂y[idx] = inv_jacobian_matrix[1, 2]
+    ∂η_∂x[idx] = inv_jacobian_matrix[2, 1]
+    ∂η_∂y[idx] = inv_jacobian_matrix[2, 2]
   end
 
-  return jacobian
+  return (; jacobian, ∂x_∂ξ, ∂x_∂η, ∂y_∂ξ, ∂y_∂η, ∂ξ_∂x, ∂η_∂x, ∂ξ_∂y, ∂η_∂y)
 end
 
-@inline function _get_fd_jacobian_matrix(mesh, (i,)::NTuple{2,Real})
+@inline function _get_fd_jacobian_matrix(mesh, (i,)::NTuple{1,Real})
   jacobian_matrix = @SMatrix [mesh.cache.∂x_∂ξ[i]]
 
   return jacobian_matrix
@@ -123,21 +139,28 @@ function _fd_metrics(ϕ::AbstractArray{T,2}, order) where {T}
   ∂ϕ_∂ξ = similar(ϕ)
   ∂ϕ_∂η = similar(ϕ)
 
+  fill!(∂ϕ_∂ξ, 0)
+  fill!(∂ϕ_∂η, 0)
+
+  ϵ = 20eps(T)
+
   iCI = @view fullCI[(begin + nhalo):(end - nhalo), :]
   jCI = @view fullCI[:, (begin + nhalo):(end - nhalo)]
 
-  @inbounds for idx in iCI
+  @batch for idx in iCI
     i, j = idx.I
     i_offset = i .+ offsets
 
-    ∂ϕ_∂ξ[idx] = sum(view(ϕ, i_offset, j) .* c_fdm.coefs)
+    _∂ϕ_∂ξ = sum(view(ϕ, i_offset, j) .* c_fdm.coefs)
+    ∂ϕ_∂ξ[idx] = _∂ϕ_∂ξ * (abs(_∂ϕ_∂ξ) >= ϵ)
   end
 
-  @inbounds for idx in jCI
+  @batch for idx in jCI
     i, j = idx.I
     j_offset = j .+ offsets
 
-    ∂ϕ_∂η[idx] = sum(view(ϕ, i, j_offset) .* c_fdm.coefs)
+    _∂ϕ_∂η = sum(view(ϕ, i, j_offset) .* c_fdm.coefs)
+    ∂ϕ_∂η[idx] = _∂ϕ_∂η * (abs(_∂ϕ_∂η) >= ϵ)
   end
 
   # ---------------------------------------------------
@@ -149,22 +172,24 @@ function _fd_metrics(ϕ::AbstractArray{T,2}, order) where {T}
   for (offset, fdm) in zip(forward_edge_offsets, forward_edge_fdms)
     edgeCI = @view fullCI[begin + nhalo - offset, :]
 
-    @inbounds for idx in edgeCI
+    @batch for idx in edgeCI
       i, j = idx.I
       i_offset = i .+ fdm.grid
 
-      ∂ϕ_∂ξ[idx] = sum(view(ϕ, i_offset, j) .* fdm.coefs)
+      _∂ϕ_∂ξ = sum(view(ϕ, i_offset, j) .* fdm.coefs)
+      ∂ϕ_∂ξ[idx] = _∂ϕ_∂ξ * (abs(_∂ϕ_∂ξ) >= ϵ)
     end
   end
 
   for (offset, fdm) in zip(backward_edge_offsets, backward_edge_fdms)
     edgeCI = @view fullCI[(end - nhalo + offset), :]
 
-    @inbounds for idx in edgeCI
+    @batch for idx in edgeCI
       i, j = idx.I
       i_offset = i .+ fdm.grid
 
-      ∂ϕ_∂ξ[idx] = sum(view(ϕ, i_offset, j) .* fdm.coefs)
+      _∂ϕ_∂ξ = sum(view(ϕ, i_offset, j) .* fdm.coefs)
+      ∂ϕ_∂ξ[idx] = _∂ϕ_∂ξ * (abs(_∂ϕ_∂ξ) >= ϵ)
     end
   end
 
@@ -174,22 +199,24 @@ function _fd_metrics(ϕ::AbstractArray{T,2}, order) where {T}
   for (offset, fdm) in zip(forward_edge_offsets, forward_edge_fdms)
     edgeCI = @view fullCI[:, begin + nhalo - offset]
 
-    @inbounds for idx in edgeCI
+    @batch for idx in edgeCI
       i, j = idx.I
       j_offset = j .+ fdm.grid
 
-      ∂ϕ_∂η[idx] = sum(view(ϕ, i, j_offset) .* fdm.coefs)
+      _∂ϕ_∂η = sum(view(ϕ, i, j_offset) .* fdm.coefs)
+      ∂ϕ_∂η[idx] = _∂ϕ_∂η * (abs(_∂ϕ_∂η) >= ϵ)
     end
   end
 
   for (offset, fdm) in zip(backward_edge_offsets, backward_edge_fdms)
     edgeCI = @view fullCI[:, (end - nhalo + offset)]
 
-    @inbounds for idx in edgeCI
+    @batch for idx in edgeCI
       i, j = idx.I
       j_offset = j .+ fdm.grid
 
-      ∂ϕ_∂η[idx] = sum(view(ϕ, i, j_offset) .* fdm.coefs)
+      _∂ϕ_∂η = sum(view(ϕ, i, j_offset) .* fdm.coefs)
+      ∂ϕ_∂η[idx] = _∂ϕ_∂η * (abs(_∂ϕ_∂η) >= ϵ)
     end
   end
 
@@ -336,4 +363,6 @@ function get_edge_fdms(nhalo, order, forward=true, deriv=1)
   end
 
   return (; edge_offsets, fdms)
+end
+
 end
