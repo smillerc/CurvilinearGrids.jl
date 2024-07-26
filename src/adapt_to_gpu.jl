53f0a898738354d90df91fe1de71934238ff6fab
Miller, Samuel <smil@lle.rochester.edu>
2024-07-26 16:58:22 -0400

    update adapt routines

--- src/adapt_to_gpu.jl
+++ src/adapt_to_gpu.jl
@@ -34,7 +34,65 @@ function Adapt.adapt_structure(to, grid::CurvilinearGrid1D)
     grid.nnodes,
     grid.domain_limits,
     grid.iterators,
+    grid.tiles,
     discretization_scheme,
+    grid.onbc,
+    grid.is_static,
+    grid.is_orthogonal,
+  )
+end
+
+function Adapt.adapt_structure(to, grid::SphericalGrid1D)
+  node_coordinates = Adapt.adapt_structure(to, grid.node_coordinates)
+  centroid_coordinates = Adapt.adapt_structure(to, grid.centroid_coordinates)
+  node_velocities = Adapt.adapt_structure(to, grid.node_velocities)
+  edge_metrics = Adapt.adapt_structure(to, grid.edge_metrics)
+  cell_center_metrics = Adapt.adapt_structure(to, grid.cell_center_metrics)
+
+  discretization_scheme = Adapt.adapt_structure(to, grid.discretization_scheme)
+
+  return SphericalGrid1D(
+    node_coordinates,
+    centroid_coordinates,
+    node_velocities,
+    edge_metrics,
+    cell_center_metrics,
+    grid.nhalo,
+    grid.nnodes,
+    grid.domain_limits,
+    grid.iterators,
+    grid.tiles,
+    discretization_scheme,
+    grid.onbc,
+    grid.snap_to_axis,
+    grid.is_static,
+    grid.is_orthogonal,
+  )
+end
+
+function Adapt.adapt_structure(to, grid::CylindricalGrid1D)
+  node_coordinates = Adapt.adapt_structure(to, grid.node_coordinates)
+  centroid_coordinates = Adapt.adapt_structure(to, grid.centroid_coordinates)
+  node_velocities = Adapt.adapt_structure(to, grid.node_velocities)
+  edge_metrics = Adapt.adapt_structure(to, grid.edge_metrics)
+  cell_center_metrics = Adapt.adapt_structure(to, grid.cell_center_metrics)
+
+  discretization_scheme = Adapt.adapt_structure(to, grid.discretization_scheme)
+
+  return CylindricalGrid1D(
+    node_coordinates,
+    centroid_coordinates,
+    node_velocities,
+    edge_metrics,
+    cell_center_metrics,
+    grid.nhalo,
+    grid.nnodes,
+    grid.domain_limits,
+    grid.iterators,
+    grid.tiles,
+    discretization_scheme,
+    grid.onbc,
+    grid.snap_to_axis,
     grid.is_static,
     grid.is_orthogonal,
   )
@@ -59,7 +117,9 @@ function Adapt.adapt_structure(to, grid::CurvilinearGrid2D)
     grid.nnodes,
     grid.domain_limits,
     grid.iterators,
+    grid.tiles,
     discretization_scheme,
+    grid.onbc,
     grid.is_static,
     grid.is_orthogonal,
   )
@@ -83,6 +143,8 @@ function Adapt.adapt_structure(to, grid::AxisymmetricGrid2D)
     grid.nhalo,
     grid.nnodes,
     grid.iterators,
+    grid.tiles,
+    discretization_scheme,
     grid.snap_to_axis,
     grid.rotational_axis,
     grid.is_static,
@@ -109,7 +171,9 @@ function Adapt.adapt_structure(to, grid::CurvilinearGrid3D)
     grid.nnodes,
     grid.domain_limits,
     grid.iterators,
+    grid.tiles,
     discretization_scheme,
+    grid.onbc,
     grid.is_static,
     grid.is_orthogonal,
   )
