1b15cdbd95b5119aad32c74095122d9fa3d53c17
Miller, Samuel <smil@lle.rochester.edu>
2024-07-29 16:56:38 -0400

    instead of having separate partitioned function calls, add keyword arguments; this will reduce duplicated code a bit

--- src/grids/simple_constructors/rtheta.jl
+++ src/grids/simple_constructors/rtheta.jl
@@ -38,12 +38,20 @@ end
 Create polar grid based on vectors of `r` and `θ` coordinates
 """
 function RThetaGrid(
-  r::AbstractVector{T}, θ::AbstractVector{T}, nhalo::Int, backend=CPU(), is_static=true
+  r::AbstractVector{T},
+  θ::AbstractVector{T},
+  nhalo::Int,
+  backend=CPU(),
+  is_static=true;
+  tile_layout=nothing,
+  rank::Int=-1,
 ) where {T}
   @assert all(r .>= 0) "Radius coordinates must be >= 0"
 
   ni = length(r)
   nj = length(θ)
+
+  cell_domain = CartesianIndices((ni - 1, nj - 1))
 
   if !all(diff(r) .> 0)
     error("Invalid r vector, spacing between vertices must be > 0 everywhere")
@@ -53,17 +61,52 @@ function RThetaGrid(
     error("Invalid θ vector, spacing between vertices must be > 0 everywhere")
   end
 
-  x = zeros(T, ni, nj)
-  y = zeros(T, ni, nj)
-
-  for j in 1:nj
-    for i in 1:ni
-      x[i, j] = r[i] * cos(θ[j])
-      y[i, j] = r[i] * sin(θ[j])
+  if !isnothing(tile_layout)
+    if rank == -1
+      error(
+        "Tile layout is provided, but rank is invalid; make sure to specify the current MPI rank",
+      )
     end
-  end
 
-  return CurvilinearGrid2D(x, y, nhalo; backend=backend, is_static=is_static)
+    if rank == 0
+      error(
+        "Rank is 0, (MPI is zero-based), but needs to be one-based, just do rank+1 for this call",
+      )
+    end
+
+    partition_fraction = max.(tile_layout, 1) # ensure no zeros or negative numbers
+    on_bc, tiled_node_limits, node_subdomain, cell_subdomain = get_subdomain_limits(
+      cell_domain, partition_fraction, rank
+    )
+
+    x = zeros(T, size(node_subdomain))
+    y = zeros(T, size(node_subdomain))
+
+    @inbounds for (gidx, lidx) in
+                  zip(node_subdomain, CartesianIndices(size(node_subdomain)))
+      i, j = lidx.I
+      gi, gj = gidx.I
+      x[i, j] = r[gi] * cos(θ[gj])
+      y[i, j] = r[gi] * sin(θ[gj])
+    end
+
+    return CurvilinearGrid2D(
+      x, y, nhalo; backend=backend, on_bc=on_bc, tiles=tiled_node_limits
+    )
+
+  else
+    x = zeros(T, ni, nj)
+    y = zeros(T, ni, nj)
+
+    for j in 1:nj
+      for i in 1:ni
+        x[i, j] = r[i] * cos(θ[j])
+        y[i, j] = r[i] * sin(θ[j])
+      end
+    end
+
+    return CurvilinearGrid2D(x, y, nhalo; backend=backend, is_static=is_static)
+  end
 end
 
 """
