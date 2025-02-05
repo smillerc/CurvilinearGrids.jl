4ea6aa762a4645764a329607cd2f32a3f967107c
Miller, Samuel <smil@lle.rochester.edu>
2025-02-05 10:18:53 -0500

    re-enable adapt to gpu backend

--- src/adapt_to_gpu.jl
+++ src/adapt_to_gpu.jl
@@ -15,110 +15,110 @@ using Adapt
 #   )
 # end
 
-# function Adapt.adapt_structure(to, grid::CurvilinearGrid1D)
-#   node_coordinates = Adapt.adapt_structure(to, grid.node_coordinates)
-#   centroid_coordinates = Adapt.adapt_structure(to, grid.centroid_coordinates)
-#   node_velocities = Adapt.adapt_structure(to, grid.node_velocities)
-#   edge_metrics = Adapt.adapt_structure(to, grid.edge_metrics)
-#   cell_center_metrics = Adapt.adapt_structure(to, grid.cell_center_metrics)
+function Adapt.adapt_structure(to, grid::CurvilinearGrid1D)
+  node_coordinates = Adapt.adapt_structure(to, grid.node_coordinates)
+  centroid_coordinates = Adapt.adapt_structure(to, grid.centroid_coordinates)
+  node_velocities = Adapt.adapt_structure(to, grid.node_velocities)
+  edge_metrics = Adapt.adapt_structure(to, grid.edge_metrics)
+  cell_center_metrics = Adapt.adapt_structure(to, grid.cell_center_metrics)
 
-#   discretization_scheme = Adapt.adapt_structure(to, grid.discretization_scheme)
+  discretization_scheme = Adapt.adapt_structure(to, grid.discretization_scheme)
 
-#   return CurvilinearGrid1D(
-#     node_coordinates,
-#     centroid_coordinates,
-#     node_velocities,
-#     edge_metrics,
-#     cell_center_metrics,
-#     grid.nhalo,
-#     grid.nnodes,
-#     grid.domain_limits,
-#     grid.iterators,
-#     grid.tiles,
-#     discretization_scheme,
-#     grid.onbc,
-#     grid.is_static,
-#     grid.is_orthogonal,
-#   )
-# end
+  return CurvilinearGrid1D(
+    node_coordinates,
+    centroid_coordinates,
+    node_velocities,
+    edge_metrics,
+    cell_center_metrics,
+    grid.nhalo,
+    grid.nnodes,
+    grid.domain_limits,
+    grid.iterators,
+    grid.tiles,
+    discretization_scheme,
+    grid.onbc,
+    grid.is_static,
+    grid.is_orthogonal,
+  )
+end
 
-# function Adapt.adapt_structure(to, grid::CurvilinearGrid2D)
-#   node_coordinates = Adapt.adapt_structure(to, grid.node_coordinates)
-#   centroid_coordinates = Adapt.adapt_structure(to, grid.centroid_coordinates)
-#   node_velocities = Adapt.adapt_structure(to, grid.node_velocities)
-#   edge_metrics = Adapt.adapt_structure(to, grid.edge_metrics)
-#   cell_center_metrics = Adapt.adapt_structure(to, grid.cell_center_metrics)
+function Adapt.adapt_structure(to, grid::CurvilinearGrid2D)
+  node_coordinates = Adapt.adapt_structure(to, grid.node_coordinates)
+  centroid_coordinates = Adapt.adapt_structure(to, grid.centroid_coordinates)
+  node_velocities = Adapt.adapt_structure(to, grid.node_velocities)
+  edge_metrics = Adapt.adapt_structure(to, grid.edge_metrics)
+  cell_center_metrics = Adapt.adapt_structure(to, grid.cell_center_metrics)
 
-#   discretization_scheme = Adapt.adapt_structure(to, grid.discretization_scheme)
+  discretization_scheme = Adapt.adapt_structure(to, grid.discretization_scheme)
 
-#   return CurvilinearGrid2D(
-#     node_coordinates,
-#     centroid_coordinates,
-#     node_velocities,
-#     edge_metrics,
-#     cell_center_metrics,
-#     grid.nhalo,
-#     grid.nnodes,
-#     grid.domain_limits,
-#     grid.iterators,
-#     grid.tiles,
-#     discretization_scheme,
-#     grid.onbc,
-#     grid.is_static,
-#     grid.is_orthogonal,
-#   )
-# end
+  return CurvilinearGrid2D(
+    node_coordinates,
+    centroid_coordinates,
+    node_velocities,
+    edge_metrics,
+    cell_center_metrics,
+    grid.nhalo,
+    grid.nnodes,
+    grid.domain_limits,
+    grid.iterators,
+    grid.tiles,
+    discretization_scheme,
+    grid.onbc,
+    grid.is_static,
+    grid.is_orthogonal,
+  )
+end
 
-# function Adapt.adapt_structure(to, grid::AxisymmetricGrid2D)
-#   node_coordinates = Adapt.adapt_structure(to, grid.node_coordinates)
-#   centroid_coordinates = Adapt.adapt_structure(to, grid.centroid_coordinates)
-#   edge_midpoint_coordinates = Adapt.adapt_structure(to, grid.edge_midpoint_coordinates)
-#   node_velocities = Adapt.adapt_structure(to, grid.node_velocities)
-#   edge_metrics = Adapt.adapt_structure(to, grid.edge_metrics)
-#   cell_center_metrics = Adapt.adapt_structure(to, grid.cell_center_metrics)
+function Adapt.adapt_structure(to, grid::AxisymmetricGrid2D)
+  node_coordinates = Adapt.adapt_structure(to, grid.node_coordinates)
+  centroid_coordinates = Adapt.adapt_structure(to, grid.centroid_coordinates)
+  edge_midpoint_coordinates = Adapt.adapt_structure(to, grid.edge_midpoint_coordinates)
+  node_velocities = Adapt.adapt_structure(to, grid.node_velocities)
+  edge_metrics = Adapt.adapt_structure(to, grid.edge_metrics)
+  cell_center_metrics = Adapt.adapt_structure(to, grid.cell_center_metrics)
 
-#   return AxisymmetricGrid2D(
-#     node_coordinates,
-#     centroid_coordinates,
-#     edge_midpoint_coordinates,
-#     node_velocities,
-#     edge_metrics,
-#     cell_center_metrics,
-#     grid.nhalo,
-#     grid.nnodes,
-#     grid.iterators,
-#     grid.tiles,
-#     grid.snap_to_axis,
-#     grid.rotational_axis,
-#     grid.onbc,
-#     grid.is_static,
-#     grid.is_orthogonal,
-#   )
-# end
+  return AxisymmetricGrid2D(
+    node_coordinates,
+    centroid_coordinates,
+    edge_midpoint_coordinates,
+    node_velocities,
+    edge_metrics,
+    cell_center_metrics,
+    grid.nhalo,
+    grid.nnodes,
+    grid.iterators,
+    grid.tiles,
+    grid.snap_to_axis,
+    grid.rotational_axis,
+    grid.onbc,
+    grid.is_static,
+    grid.is_orthogonal,
+  )
+end
 
-# function Adapt.adapt_structure(to, grid::CurvilinearGrid3D)
-#   node_coordinates = Adapt.adapt_structure(to, grid.node_coordinates)
-#   centroid_coordinates = Adapt.adapt_structure(to, grid.centroid_coordinates)
-#   node_velocities = Adapt.adapt_structure(to, grid.node_velocities)
-#   edge_metrics = Adapt.adapt_structure(to, grid.edge_metrics)
-#   cell_center_metrics = Adapt.adapt_structure(to, grid.cell_center_metrics)
+function Adapt.adapt_structure(to, grid::CurvilinearGrid3D)
+  node_coordinates = Adapt.adapt_structure(to, grid.node_coordinates)
+  centroid_coordinates = Adapt.adapt_structure(to, grid.centroid_coordinates)
+  node_velocities = Adapt.adapt_structure(to, grid.node_velocities)
+  edge_metrics = Adapt.adapt_structure(to, grid.edge_metrics)
+  cell_center_metrics = Adapt.adapt_structure(to, grid.cell_center_metrics)
 
-#   discretization_scheme = Adapt.adapt_structure(to, grid.discretization_scheme)
+  discretization_scheme = Adapt.adapt_structure(to, grid.discretization_scheme)
 
-#   return CurvilinearGrid3D(
-#     node_coordinates,
-#     centroid_coordinates,
-#     node_velocities,
-#     edge_metrics,
-#     cell_center_metrics,
-#     grid.nhalo,
-#     grid.nnodes,
-#     grid.domain_limits,
-#     grid.iterators,
-#     grid.tiles,
-#     discretization_scheme,
-#     grid.onbc,
-#     grid.is_static,
-#     grid.is_orthogonal,
-#   )
-# end
+  return CurvilinearGrid3D(
+    node_coordinates,
+    centroid_coordinates,
+    node_velocities,
+    edge_metrics,
+    cell_center_metrics,
+    grid.nhalo,
+    grid.nnodes,
+    grid.domain_limits,
+    grid.iterators,
+    grid.tiles,
+    discretization_scheme,
+    grid.onbc,
+    grid.is_static,
+    grid.is_orthogonal,
+  )
+end
