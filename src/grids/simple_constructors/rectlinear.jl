eba512cf11d02967dc99edd90a248715c5257744
Aiden Rohrbach <arohrbachj@protonmail.com>
2025-05-01 12:42:39 -0400

    Add sleep for benchmarking purposes

--- src/grids/simple_constructors/rectlinear.jl
+++ src/grids/simple_constructors/rectlinear.jl
@@ -159,7 +159,7 @@ function rectlinear_grid(
       "The number of cells specified must be > 1, given cell dims are $((ni_cells, nj_cells))",
     )
   end
-  sleep(0.1)
+  sleep(5)
 
   return rectlinear_grid(
     range(x0, x1; length=ni_cells + 1) .|> T,
