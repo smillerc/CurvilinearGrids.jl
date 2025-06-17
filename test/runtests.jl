85f022d6ae2431830598b8337fc0171c4dcd724d
Miller, Samuel <smil@lle.rochester.edu>
2025-06-17 15:43:22 -0400

    test updates

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using CurvilinearGrids
+using CurvilinearGrids.RectlinearArrays
 using Test
 using StaticArrays
 using BenchmarkTools
@@ -7,15 +8,18 @@ using StructArrays
 using WriteVTK
 using UnPack
 
+const test_gpu = false
+
 include("unit/common.jl")
 @testset verbose = true "UnitTests" begin
   include("unit/test_1d.jl")
 
   include("unit/test_2d.jl")
+  include("unit/test_rectlinear_arrays.jl")
   include("unit/test_2d_axisymmetric.jl")
   include("unit/perturb_mesh.jl")
 
-  include("unit/test_3d.jl")
+  # include("unit/test_3d.jl")
   include("unit/test_wall.jl")
   # include("unit/test_meg6.jl")
 end
