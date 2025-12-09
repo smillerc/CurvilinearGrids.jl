45a1f00c14bf27ff7a175a1e8b93197ee9053cc9
Sam Miller <smil@lle.rochester.edu>
2025-12-09 10:47:57 -0500

    expanding cell/edge operators more

--- src/grids/3d_spherical.jl
+++ src/grids/3d_spherical.jl
@@ -1,6 +1,3 @@
-module SphericalGrid
-
-using CartesianDomains
 
 struct SphericalGrid3D{NC,CNC,CC,CV,I,DL,FA} <: AbstractCurvilinearGrid3D
   node_coordinates::NC
@@ -16,8 +13,6 @@ end
 function SphericalGrid3D(
   r::AbstractVector{T}, θ::AbstractVector{T}, ϕ::AbstractVector{T}, nhalo::Int, backend
 ) where {T<:Real}
-
-  #
   nr, ntheta, nphi = (length(r) + 2nhalo, length(θ) + 2nhalo, length(ϕ) + 2nhalo)
   halo_coords_included = true # the node dims include halo
   limits, domain_iterators = get_iterators((nr, ntheta, nphi), halo_coords_included, nhalo)
@@ -242,23 +237,79 @@ end
   z[I] = rr * cos(th)
 end
 
-#
+# --------------------------------------------------------------------
+# Geometry helpers
+# --------------------------------------------------------------------
+
+@inline cell_volume(mesh::SphericalGrid3D, I::CartesianIndex{3}) =
+  @inbounds mesh.cell_volumes[I]
+
+@inline function face_area_p(mesh::SphericalGrid3D, I::CartesianIndex{3}, axis::Int)
+  @inbounds if axis == 1
+    return mesh.face_areas.i₊½[I]
+  elseif axis == 2
+    return mesh.face_areas.j₊½[I]
+  else
+    return mesh.face_areas.k₊½[I]
+  end
+end
+
+@inline function face_area_m(mesh::SphericalGrid3D, I::CartesianIndex{3}, axis::Int)
+  I₋ = CartesianDomains.shift(I, axis, -1)
+  @inbounds if axis == 1
+    return mesh.face_areas.i₊½[I₋]  # i-1/2
+  elseif axis == 2
+    return mesh.face_areas.j₊½[I₋]  # j-1/2
+  else
+    return mesh.face_areas.k₊½[I₋]  # k-1/2
+  end
+end
+
+@inline function face_val(A, I₁::CartesianIndex{3}, I₂::CartesianIndex{3})
+  @inbounds (1 / 2) * (A[I₁] + A[I₂])
+end
+
+# Effective physical spacing at edge between I and I+ê_axis
+@inline function Δx_eff_edge(mesh::SphericalGrid3D, I::CartesianIndex{3}, axis::Int)
+  I₊ = CartesianDomains.shift(I, axis, +1)
+  V_L = cell_volume(mesh, I)
+  V_R = cell_volume(mesh, I₊)
+  A_n = face_area_p(mesh, I, axis)
+  return (V_L + V_R) / A_n
+end
+
+# --------------------------------------------------------------------
 # Cell-center operators
-#
+# --------------------------------------------------------------------
 
 function cell_center_derivative(
   mesh::SphericalGrid3D, A::AbstractArray{T,3}, I::CartesianIndex{3}, axis::Int
 ) where {T}
+  I₊ = CartesianDomains.shift(I, axis, +1)
+  I₋ = CartesianDomains.shift(I, axis, -1)
 
-  #
-  ᵢ₊₁ = CartesianDomains.shift(I, axis, +1)
+  @inbounds A₊ = A[I₊]
   @inbounds Aᵢ = A[I]
-  @inbounds Aᵢ₊₁ = A[ᵢ₊₁]
-  # ∂A = ...
+  @inbounds A₋ = A[I₋]
+
+  Vᵢ = cell_volume(mesh, I)
+  V₊ = cell_volume(mesh, I₊)
+  V₋ = cell_volume(mesh, I₋)
+
+  A_face_p = face_area_p(mesh, I, axis)
+  A_face_m = face_area_m(mesh, I, axis)
+
+  Δx_f = (Vᵢ + V₊) / A_face_p    # forward spacing
+  Δx_b = (V₋ + Vᵢ) / A_face_m    # backward spacing
+
+  ∂A_f = (A₊ - Aᵢ) / Δx_f
+  ∂A_b = (Aᵢ - A₋) / Δx_b
+
+  ∂A = (1 / 2) * (∂A_f + ∂A_b)
   return ∂A
 end
 
-# ∇A at the cell center
+# ∇A at the cell center (physical gradient)
 function cell_center_gradient(
   mesh::SphericalGrid3D, A::AbstractArray{T,3}, I::CartesianIndex{3}
 ) where {T}
@@ -268,44 +319,154 @@ function cell_center_gradient(
   return @SVector [∂A∂r, ∂A∂θ, ∂A∂ϕ]
 end
 
-# ∇×A at the cell center
-function cell_center_curl(
-  mesh::SphericalGrid3D,
-  (Ar::AbstractArray{T,3}, Aθ::AbstractArray{T,3}, Aϕ::AbstractArray{T,3}),
-  I::CartesianIndex{3},
-  axis::Int,
-) where {T}
-  ∇Ar = cell_center_gradient(mesh, Ar, I)
-  ∇Aθ = cell_center_gradient(mesh, Aθ, I)
-  ∇Aϕ = cell_center_gradient(mesh, Aϕ, I)
-  return curl_A
-end
-
-# ∇⋅A at the cell center
+# ∇⋅A at the cell center (A_r,A_θ,A_ϕ are physical components)
 function cell_center_divergence(
-  mesh::SphericalGrid3D, A::AbstractArray{T,3}, I::CartesianIndex{3}
+  mesh::SphericalGrid3D,
+  (A_r::AbstractArray{T,3}, A_θ::AbstractArray{T,3}, A_ϕ::AbstractArray{T,3}),
+  I::CartesianIndex{3},
 ) where {T}
-  ∇A = cell_center_gradient(mesh, A, I)
+  V = cell_volume(mesh, I)
+
+  # convenience neighbor indices
+  I_i₊ = CartesianDomains.shift(I, 1, +1)
+  I_i₋ = CartesianDomains.shift(I, 1, -1)
+  I_j₊ = CartesianDomains.shift(I, 2, +1)
+  I_j₋ = CartesianDomains.shift(I, 2, -1)
+  I_k₊ = CartesianDomains.shift(I, 3, +1)
+  I_k₋ = CartesianDomains.shift(I, 3, -1)
+
+  # radial faces
+  @inbounds A_rᵢ = A_r[I]
+  @inbounds A_rᵢ₊₁ = A_r[I_i₊]
+  @inbounds A_rᵢ₋₁ = A_r[I_i₋]
+
+  A_r_face_p = (1 / 2) * (A_rᵢ + A_rᵢ₊₁)
+  A_r_face_m = (1 / 2) * (A_rᵢ₋₁ + A_rᵢ)
+
+  A_r_area_p = face_area_p(mesh, I, 1)
+  A_r_area_m = face_area_m(mesh, I, 1)
+
+  flux_r = A_r_face_p * A_r_area_p - A_r_face_m * A_r_area_m
+
+  # theta faces
+  @inbounds A_θⱼ = A_θ[I]
+  @inbounds A_θⱼ₊₁ = A_θ[I_j₊]
+  @inbounds A_θⱼ₋₁ = A_θ[I_j₋]
+
+  A_θ_face_p = (1 / 2) * (A_θⱼ + A_θⱼ₊₁)
+  A_θ_face_m = (1 / 2) * (A_θⱼ₋₁ + A_θⱼ)
+
+  A_θ_area_p = face_area_p(mesh, I, 2)
+  A_θ_area_m = face_area_m(mesh, I, 2)
+
+  flux_θ = A_θ_face_p * A_θ_area_p - A_θ_face_m * A_θ_area_m
+
+  # phi faces
+  @inbounds A_ϕₖ = A_ϕ[I]
+  @inbounds A_ϕₖ₊₁ = A_ϕ[I_k₊]
+  @inbounds A_ϕₖ₋₁ = A_ϕ[I_k₋]
+
+  A_ϕ_face_p = (1 / 2) * (A_ϕₖ + A_ϕₖ₊₁)
+  A_ϕ_face_m = (1 / 2) * (A_ϕₖ₋₁ + A_ϕₖ)
+
+  A_ϕ_area_p = face_area_p(mesh, I, 3)
+  A_ϕ_area_m = face_area_m(mesh, I, 3)
+
+  flux_ϕ = A_ϕ_face_p * A_ϕ_area_p - A_ϕ_face_m * A_ϕ_area_m
+
+  div_A = (flux_r + flux_θ + flux_ϕ) / V
   return div_A
 end
 
-#
+# ∇×A at the cell center (physical components)
+function cell_center_curl(
+  mesh::SphericalGrid3D,
+  (A_r::AbstractArray{T,3}, A_θ::AbstractArray{T,3}, A_ϕ::AbstractArray{T,3}),
+  I::CartesianIndex{3},
+) where {T}
+  V = cell_volume(mesh, I)
+
+  I_i₊ = CartesianDomains.shift(I, 1, +1)
+  I_i₋ = CartesianDomains.shift(I, 1, -1)
+  I_j₊ = CartesianDomains.shift(I, 2, +1)
+  I_j₋ = CartesianDomains.shift(I, 2, -1)
+  I_k₊ = CartesianDomains.shift(I, 3, +1)
+  I_k₋ = CartesianDomains.shift(I, 3, -1)
+
+  # ω_r: circulation in (θ,ϕ)
+  A_ϕ_face_θp = face_val(A_ϕ, I, I_j₊)
+  A_ϕ_face_θm = face_val(A_ϕ, I_j₋, I)
+
+  A_θ_face_ϕp = face_val(A_θ, I, I_k₊)
+  A_θ_face_ϕm = face_val(A_θ, I_k₋, I)
+
+  A_θ_area_p = face_area_p(mesh, I, 2)
+  A_θ_area_m = face_area_m(mesh, I, 2)
+  A_ϕ_area_p = face_area_p(mesh, I, 3)
+  A_ϕ_area_m = face_area_m(mesh, I, 3)
+
+  circ_r =
+    (A_θ_area_p * A_ϕ_face_θp - A_θ_area_m * A_ϕ_face_θm) -
+    (A_ϕ_area_p * A_θ_face_ϕp - A_ϕ_area_m * A_θ_face_ϕm)
+
+  ω_r = circ_r / V
+
+  # ω_θ: circulation in (ϕ,r)
+  A_r_face_ϕp = face_val(A_r, I, I_k₊)
+  A_r_face_ϕm = face_val(A_r, I_k₋, I)
+
+  A_ϕ_face_rp = face_val(A_ϕ, I, I_i₊)
+  A_ϕ_face_rm = face_val(A_ϕ, I_i₋, I)
+
+  A_ϕ_area_p2 = face_area_p(mesh, I, 3)
+  A_ϕ_area_m2 = face_area_m(mesh, I, 3)
+  A_r_area_p = face_area_p(mesh, I, 1)
+  A_r_area_m = face_area_m(mesh, I, 1)
+
+  circ_θ =
+    (A_ϕ_area_p2 * A_r_face_ϕp - A_ϕ_area_m2 * A_r_face_ϕm) -
+    (A_r_area_p * A_ϕ_face_rp - A_r_area_m * A_ϕ_face_rm)
+
+  ω_θ = circ_θ / V
+
+  # ω_ϕ: circulation in (r,θ)
+  A_θ_face_rp2 = face_val(A_θ, I, I_i₊)
+  A_θ_face_rm2 = face_val(A_θ, I_i₋, I)
+
+  A_r_face_θp2 = face_val(A_r, I, I_j₊)
+  A_r_face_θm2 = face_val(A_r, I_j₋, I)
+
+  A_r_area_p2 = face_area_p(mesh, I, 1)
+  A_r_area_m2 = face_area_m(mesh, I, 1)
+  A_θ_area_p2 = face_area_p(mesh, I, 2)
+  A_θ_area_m2 = face_area_m(mesh, I, 2)
+
+  circ_ϕ =
+    (A_r_area_p2 * A_θ_face_rp2 - A_r_area_m2 * A_θ_face_rm2) -
+    (A_θ_area_p2 * A_r_face_θp2 - A_θ_area_m2 * A_r_face_θm2)
+
+  ω_ϕ = circ_ϕ / V
+
+  return @SVector [ω_r, ω_θ, ω_ϕ]
+end
+
+# --------------------------------------------------------------------
 # Edge operators
-#
+# --------------------------------------------------------------------
 
 function edge_derivative(
   mesh::SphericalGrid3D, A::AbstractArray{T,3}, I::CartesianIndex{3}, axis::Int
 ) where {T}
-
-  #
-  ᵢ₊₁ = CartesianDomains.shift(I, axis, +1)
+  I₊ = CartesianDomains.shift(I, axis, +1)
   @inbounds Aᵢ = A[I]
-  @inbounds Aᵢ₊₁ = A[ᵢ₊₁]
-  # ∂A = ...
+  @inbounds A₊ = A[I₊]
+
+  Δx = Δx_eff_edge(mesh, I, axis)
+  ∂A = (A₊ - Aᵢ) / Δx
   return ∂A
 end
 
-# ∇A at the edge center
+# ∇A at the edge center (full physical gradient)
 function edge_gradient(
   mesh::SphericalGrid3D, A::AbstractArray{T,3}, I::CartesianIndex{3}
 ) where {T}
@@ -315,25 +476,34 @@ function edge_gradient(
   return @SVector [∂A∂r, ∂A∂θ, ∂A∂ϕ]
 end
 
-# ∇×A at the edge center
-function edge_curl(
+# ∇⋅A at the edge center = average of adjacent cell-center divergences
+function edge_divergence(
   mesh::SphericalGrid3D,
-  (Ar_edge::AbstractArray{T,3}, Aθ_edge::AbstractArray{T,3}, Aϕ_edge::AbstractArray{T,3}),
+  (A_r::AbstractArray{T,3}, A_θ::AbstractArray{T,3}, A_ϕ::AbstractArray{T,3}),
   I::CartesianIndex{3},
   edge_axis::Int,
 ) where {T}
-  ∇Ar_edge = edge_gradient(mesh, Ar_edge, I)
-  ∇Aθ_edge = edge_gradient(mesh, Aθ_edge, I)
-  ∇Aϕ_edge = edge_gradient(mesh, Aϕ_edge, I)
-  return curl_A
+  I₊ = CartesianDomains.shift(I, edge_axis, +1)
+
+  div_L = cell_center_divergence(mesh, (A_r, A_θ, A_ϕ), I)
+  div_R = cell_center_divergence(mesh, (A_r, A_θ, A_ϕ), I₊)
+
+  return (1 / 2) * (div_L + div_R)
 end
 
-# ∇⋅A at the cell center
-function edge_divergence(
-  mesh::SphericalGrid3D, A::AbstractArray{T,3}, I::CartesianIndex{3}, edge_axis::Int
+# ∇×A at the edge center = average of adjacent cell-center curls
+function edge_curl(
+  mesh::SphericalGrid3D,
+  (
+    A_r_edge::AbstractArray{T,3}, A_θ_edge::AbstractArray{T,3}, A_ϕ_edge::AbstractArray{T,3}
+  ),
+  I::CartesianIndex{3},
+  edge_axis::Int,
 ) where {T}
-  ∇A = edge_gradient(mesh, A, I)
-  return div_A
-end
+  I₊ = CartesianDomains.shift(I, edge_axis, +1)
 
-end # module
+  curl_L = cell_center_curl(mesh, (A_r_edge, A_θ_edge, A_ϕ_edge), I)
+  curl_R = cell_center_curl(mesh, (A_r_edge, A_θ_edge, A_ϕ_edge), I₊)
+
+  return (1 / 2) * (curl_L + curl_R)
+end
