ea8f7d23cf28708b7b3126ca195e20519a9a3f61
Sam Miller <smil@lle.rochester.edu>
2026-01-12 12:17:35 -0500

    add `adapt_structure` for `SphericalGrid3D`

--- src/adapt_to_gpu.jl
+++ src/adapt_to_gpu.jl
@@ -162,3 +162,42 @@ function Adapt.adapt_structure(to, grid::CurvilinearGrid3D)
     grid.halo_coords_included,
   )
 end
+
+function Adapt.adapt_structure(to, grid::SphericalGrid3D)
+  node_coordinates = Adapt.adapt_structure(to, grid.node_coordinates)
+  cartesian_node_coordinates = Adapt.adapt_structure(to, grid.cartesian_node_coordinates)
+  centroid_coordinates = Adapt.adapt_structure(to, grid.centroid_coordinates)
+  cell_volumes = Adapt.adapt_structure(to, grid.cell_volumes)
+  face_areas = Adapt.adapt_structure(to, grid.face_areas)
+
+  # cartesian_node_coords = (;
+  #   x=KernelAbstractions.zeros(backend, T, nodedims),
+  #   y=KernelAbstractions.zeros(backend, T, nodedims),
+  #   z=KernelAbstractions.zeros(backend, T, nodedims),
+  # )
+
+  # spherical_centroid_coords = (;
+  #   r=KernelAbstractions.zeros(backend, T, celldims[1]),
+  #   θ=KernelAbstractions.zeros(backend, T, celldims[2]),
+  #   ϕ=KernelAbstractions.zeros(backend, T, celldims[3]),
+  # )
+
+  # cell_volumes = KernelAbstractions.zeros(backend, T, celldims)
+
+  # face_areas = (;
+  #   i₊½=KernelAbstractions.zeros(backend, T, celldims),
+  #   j₊½=KernelAbstractions.zeros(backend, T, celldims),
+  #   k₊½=KernelAbstractions.zeros(backend, T, celldims),
+  # )
+
+  return SphericalGrid3D(
+    node_coordinates,
+    cartesian_node_coordinates,
+    centroid_coordinates,
+    cell_volumes,
+    grid.iterators,
+    grid.domain_limits,
+    face_areas,
+    grid.nhalo,
+  )
+end
